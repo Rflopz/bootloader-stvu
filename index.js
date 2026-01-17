3b994f2c30469befeaaf77718d87a66f6285e980
rlopez <rlopez@rtafleet.com>
2026-01-17 09:55:05 -0700

    init a vue app

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
   import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+  import App from './app.vue'
 
   async function bootstrap() {
     const app = document.createElement('div#app')
     document.body.appendChild(app)
 
-    const vue = {
-      template: `
-        <div>
-          <h1>Hello World</h1>
-        </div>
-      `,
-      setup() {
-        onMounted(() => {
-          console.log('vue mounted')
-        })
-      }
-    }
-
-    createApp(vue).mount(app)
+    createApp(App).mount(app)
   }
 
   bootstrap()
